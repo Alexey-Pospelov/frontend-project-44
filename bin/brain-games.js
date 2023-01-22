cd8860080ced25f1ce1baca5b98ca76035801800
trainer81 <nch54052@rambler.ru>
2023-01-22 15:45:09 +0300

    change brain-eve.js

---
+++ bin/brain-games.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+// eslint-disable-next-line import/no-absolute-path
+import hello from '/home/trainer81/frontend-project-44/src/cli.js';
+
+console.log('Welcome to the Brain Games!');
+
+console.log(hello());
